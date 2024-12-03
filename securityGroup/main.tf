a85b129fada6a5383828f82c4611d46eaa7247c8
Abdul <abdul@intodepth.in>
2024-12-03 17:10:35 +0530

    graph

--- securityGroup/main.tf
+++ securityGroup/main.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "DatabaseSg" {
     cidr_blocks = var.PubSubnetcidr
   }
 
+    ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
